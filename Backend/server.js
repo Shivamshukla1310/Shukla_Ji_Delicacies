c504efc1aea0121f8673c957c60a0975a9713331
Shivamshukla1310 <shukla.shivamv@gmail.com>
2025-11-22 19:44:49 +0530

    Everything Here Works Completely Fine Without Any Problem Backend Is Finally Done

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
@@ -8,42 +7,21 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
+// DB connect
 connectDb();
 
-// Middleware - order matters
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// serve static files (images)
+// Serve static files
 app.use("/images", express.static(path.join(__dirname, "public", "images")));
 
-// Routes
-app.use("/", require("./routes/user"));
+// ROUTES (VERY IMPORTANT ORDER!)
+app.use("/user", require("./routes/user"));
 app.use("/recipe", require("./routes/recipe"));
 
 app.listen(PORT, () => {
   console.log(`App is listening at ${PORT}`);
 });
-
-
-
-// const express = require("express")
-// const app = express()
-// const dotenv = require("dotenv").config()
-// const connectDb = require("./config/connectionDb")
-// const cors = require("cors")
-
-// const PORT = process.env.PORT || 3000
-// connectDb()
-
-// app.use(express.json())
-// app.use(cors())
-// app.use(express.static("public"))
-
-// app.use("/", require("./routes/user"))
-// app.use("/recipe", require("./routes/recipe"))
-
-// app.listen(PORT, (err) => {
-//   console.log(`app is listening on port ${PORT}`)
-// })
